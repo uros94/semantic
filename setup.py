ed8fc1cb627200c7153e53d5687486a33ba445a9
uros94 <uros.ng@gmail.com>
2018-05-12 15:36:34 +0200

    added classes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='semantic',
+      version='0.1',
+      description='The semantic analyser',
+      url='https://github.com/uros94/semantic',
+      author='Flying Circus',
+      author_email='uros.ng@gmail.com',
+      license='MIT',
+      packages=['semantic'],
+      zip_safe=False)
